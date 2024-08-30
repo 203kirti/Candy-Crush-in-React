44fd4c9232c2ca998d8eb085070b3081038b00f1
Kirti <kirti.air203@gmail.com>
2024-08-30 23:39:33 +0530

    initial commit

---
+++ src/components/MaxScore.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MaxScore = ({score}) => {
+    return (
+    <div className="score-card">
+       <h2>  Current Score 👉🏼: {score}</h2> 
+    </div>
+    )
+}
+
+export default MaxScore;
